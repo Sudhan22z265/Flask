4bd5e871be0fb22655551227b71e9aaa34dc4e9f
Your Name <you@example.com>
2024-01-21 00:13:19 +0530

    added templates and jinja2 uses

--- server.py
+++ server.py
@@ -1,13 +1,13 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>hello</h1>'
+    return render_template('index.html')
 
 @app.route('/user/<name>')
 def index1(name):
-    return f'<h1>hello{name}</h1>'
+    return render_template('user.html',name=name)
 
 if __name__ == '__main__':
     app.run(debug=True) #debug reloads application after changes and show error detailly
