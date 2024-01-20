8d895f2489d4c09b3f42ed4153fb47d4e6515428
Your Name <you@example.com>
2024-01-21 00:00:27 +0530

    dynamic routes using angular bkts <>

--- server.py
+++ server.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def index():
     return '<h1>hello</h1>'
 
+@app.route('/user/<name>')
+def index1(name):
+    return f'<h1>hello{name}</h1>'
+
 if __name__ == '__main__':
     app.run(debug=True) #debug reloads application after changes and show error detailly
