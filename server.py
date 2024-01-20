3c5bee6e7a8d725b5e6c5a10e6790c0e573ff31c
Your Name <you@example.com>
2024-01-21 00:24:49 +0530

    added control and for loop and json

--- server.py
+++ server.py
@@ -1,13 +1,15 @@
 from flask import Flask,render_template
+import json
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return json.dumps({'name':'name'})
 
 @app.route('/user/<name>')
 def index1(name):
-    return render_template('user.html',name=name)
+    fruits = ['apple','orange']
+    return render_template('user.html',name=name,fruits=fruits)
 
 if __name__ == '__main__':
     app.run(debug=True) #debug reloads application after changes and show error detailly
